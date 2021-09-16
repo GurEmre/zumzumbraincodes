2ece3d10997d457b507498ab2d00789eeb19ae0e
GurEmre <guremreme@gmail.com>
2021-09-16 03:22:12 +0300

    second

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Here we have raspi codes
+
+Need library first:
+
+sudo apt-get install git build-essential python-dev
+cd ~
+git clone https://github.com/adafruit/Adafruit_Python_PCA9685.git
+cd Adafruit_Python_PCA9685
+sudo python setup.py install
+
+here a good example:
+https://www.youtube.com/watch?v=9jcEwn7GzNs
--- servotry.py
+++ servotry.py
@@ -1,1 +1,19 @@
-asdfasdf
+#simple test icine bak
+
+pwm.set_pwm_freq(60)
+
+while True:
+    pwm.set_pwm(0,0,servo_min)
+    time.sleep(1)
+    pwm.set_pwm(0,0,servo_max)
+    time.sleep(1)
+
+    pwm.set_pwm(1,0,servo_min)
+    time.sleep(1)
+    pwm.set_pwm(1,0,servo_max)
+    time.sleep(1)
+
+    pwm.set_pwm(2,0,servo_min)
+    time.sleep(1)
+    pwm.set_pwm(2,0,servo_max)
+    time.sleep(1)
