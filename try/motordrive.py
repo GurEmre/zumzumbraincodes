f6ba56eb0ed591290176f08a647c89b0457096d8
GurEmre <guremreme@gmail.com>
2021-09-17 04:54:26 +0300

    all

---
+++ try/motordrive.py
@@ -0,0 +1,31 @@
+
+import struct
+import smbus
+bus=smbus.SMBus(1)
+
+def parse_data(address, register, num_bytes):
+        """
+        Parse multiple bytes long message by using I2C
+        """
+        try:
+            read_block = bus.read_i2c_block_data(address, register, num_bytes)
+        except IOError:
+            print("IOerr")
+            return None
+            # If message have error use the last value
+        byte_list = ''
+        for byte in read_block:
+            byte_list += chr(byte)
+        # reverse ordering of incoming data
+        byte_list = byte_list[::-1]
+        try:
+            # unpack value obtained according to the struct_type
+            # the result of unpack is a tuple get the first item
+            val = struct.unpack("=h", byte_list)[0]
+        except struct.error:
+            # If message have error use the last value
+            print("I2C value Error")
+            return None
+        return val
+# bus.write_byte(8, 255)
+print(parse_data(8, 255, 2))
---
+++ try/servotry.py
@@ -0,0 +1,38 @@
+
+from __future__ import division
+import time
+
+import Adafruit_PCA9685
+
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
+# Alternatively specify a different address and/or bus:
+#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+
+servo_min = 150  # Change to find suitable servo pos.
+servo_max = 400  
+
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
+pwm.set_pwm_freq(60)
+
+print('Moving servo on channel 0, press Ctrl-C to quit...')
+while True:
+    pwm.set_pwm(0, 0, servo_min)
+    time.sleep(1)
+    pwm.set_pwm(0, 0, servo_max)
+    time.sleep(1)
