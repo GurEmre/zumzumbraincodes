b7d843fe8429cf6d2a47b54e3e7c023909b6db7f
GurEmre <guremreme@gmail.com>
2021-12-02 17:07:58 +0300

    dfvadsfsd

--- try/distance.py
+++ try/distance.py
@@ -1,92 +1,132 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import sys
-import numpy as np
-sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+#!/usr/bin/env python
 import cv2
-import cv2.aruco as aruco
-import numpy as np
-#import rectangleArea as ra
 import math
+import numpy as np
+import tf
+import sys
+from kobot.msg import rt
+from std_msgs.msg import String, UInt8
+import rospy
+# for publishing dictionary as encoded string
+import json
+
+class landmarkTfBroadcaster:
+
+    def __init__(self):
+        # pub landmark ID to wake up subscriber for landmark tf
+        self.landmark_pub = rospy.Publisher("/sensors/landmark_sensor",
+            UInt8, queue_size=1)
+        self.broadcaster = tf.TransformBroadcaster()
+        self.listener = tf.TransformListener()
+        self.landmark_tf_dict = {}
+        self.rt_sub = rospy.Subscriber("rt",
+            rt, self.rt_callback, queue_size=1)
+
+    def rt_callback(self, data):
+        """
+        Get rvec and tvec from aruco detector's published msg
+        send tf b/w cam_link and detected aruco
+        """
+        # we need a homogeneous matrix but 
+        # OpenCV only gives us a 3x3 rotation matrix
+        rotation_matrix = np.array([[0, 0, 0, 0],
+                                    [0, 0, 0, 0],
+                                    [0, 0, 0, 0],
+                                    [0, 0, 0, 1]],
+                                    dtype=float)
+
+        rotation_matrix[:3, :3], _ = cv2.Rodrigues(data.rvec)
+
+        # convert the matrix to euler angles 
+        euler = tf.transformations.euler_from_matrix(rotation_matrix, 'rxyz')
+        if abs(euler[1]) > self.max_ang:
+            # we are looking from a problematic angle
+            # ignore it
+            return
+
+        # euler angle sequence to look like base link frame
+        #  and only rotate on z axis of map frame
+        quaternion = tf.transformations.quaternion_from_euler(
+            math.pi/2,
+            math.pi/2,
+            euler[1],
+            'ryxz')
+        rospy.loginfo(euler[1]*180.0/math.pi)
+
+        current_time = rospy.Time.now()
+        self.broadcaster.sendTransform(
+            data.tvec,
+            quaternion,
+            current_time,                   # timestamp
+            "landmark{}".format(data.id),   # child frame
+            "cam_link")                     # parent frame
+
+        # trans, rot = self.lookup_tf("odom", "landmark{}".format(data.id))
+        # # add tf to dict. to republish later
+        # self.landmark_tf_dict[data.id] = [trans, rot]
+        # # publish landmark to lba driver
+        # landmark_id_msg = UInt8()
+        # landmark_id_msg = data.id
+        # self.landmark_pub.publish(landmark_id_msg)
+
+    def get_params(self):
+        """
+        LBA params. are checked constantly and is
+        updated if necessary
+        """
+        if rospy.has_param('lba_params'):
+            # fetch a group (dictionary) of parameters
+            params = rospy.get_param('lba_params')
+            self.max_ang = params['max_ang']
+
+        else:  # feed default vals
+            self.max_ang = math.pi
+
+    def lookup_tf(self, parent_frame, child_frame):
+        try:
+            self.listener.waitForTransform(
+                child_frame,
+                parent_frame,
+                rospy.Time(0),
+                rospy.Duration(1.0))
+            (trans, rot) = self.listener.lookupTransform(
+                parent_frame, child_frame, rospy.Time(0))
+            return [trans, rot]
+        except (tf.LookupException, tf.ConnectivityException,
+                tf.ExtrapolationException):
+            rospy.logerr("TF from {} to {} N/A landmark TF".format(
+                parent_frame,
+                child_frame))
+
+    def repub_landmarks(self):
+        """
+        Republish landmark locations
+        in odom frame periodically
+        otherwise tf package disregards tfs 
+        older than 10s
+        """
+        # publish odom to landmark tf to not being deleted
+        for key,val in self.landmark_tf_dict.items():
+            current_time = rospy.Time.now()
+            self.broadcaster.sendTransform(
+                val[0],
+                val[1],
+                current_time,                   # timestamp
+                "landmark{}".format(key),       # child frame
+                "odom")                         # parent frame
 
 
-# reding calibration matrices
+def start(args):
+    rospy.init_node('landmark_tf', anonymous=True)
 
-calibrationResultPath = "res//"
+    ltf = landmarkTfBroadcaster()
+    # set period to 5s to ensure tfs are not deleted
+    rate = rospy.Rate(0.5)
+    while not rospy.is_shutdown():
+        ltf.get_params()
+        ltf.repub_landmarks()
+        rate.sleep()
+        
 
-fsContent = cv2.FileStorage(calibrationResultPath + "calibrationValues0.yaml", cv2.FILE_STORAGE_READ)
-
-mtxNode = fsContent.getNode('camera_matrix')
-distNode = fsContent.getNode('dist_coeff')
-
-mtx = np.asarray(mtxNode.mat())
-distor = np.asarray(distNode.mat())
-
-print("--------------------")
-print(mtx)
-print("--------------------")
-print(distor)
-print("--------------------")
-
-#mtx = np.array([[2.6822003708394282e+03, 0., 1.5588865381021240e+03], [0., 2.6741978758743703e+03, 1.2303469240154550e+03], [0., 0., 1.]])
-#distor = np.array([2.0426196677407879e-01, -3.3902097431574091e-01, -4.1813964792274307e-03, -1.0425257413809015e-02, 8.2004709580884308e-02])
-
-# getting ready the aruco dictionary
-
-aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-
-# opening rpi camera
-
-camera = PiCamera()
-#camera.resolution = (1040, 784)
-camera.resolution = (1024, 770)
-camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(1024, 770))
-
-time.sleep(0.1)
-
-tvec0 = np.array([[[0.0, 0.0, 0.0]]])
-
-rvecmax = 0.0
-
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
-    cv2.rectangle(image, (0, 0), (200, 200), (220, 240, 230), -1)
-
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(image, aruco_dict)
-    image = aruco.drawDetectedMarkers(image, corners) # marker krvonalak
-
-      
-    rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, distor)
-
-    #rvec = np.array([[[0.0, 0.0, 0.0]]])
-
-  
-    if ids is not None:
-        for i in range(0, ids.size):
-            #print(image.dtype)
- #           rr, thet = ra.rArea(corners)
-            aruco.drawAxis(image, mtx, distor, rvec[0], tvec[0], 0.06) # np.array([0.0, 0.0, 0.0])
-            #print(f, "\t", end = " ")
-            #cv2.putText(image, image.shape())
-            #cv2.putText(image, "%.1f cm" % ((20000 / rr**0.5) * 0.116 - 2.08), (0, 230), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (244, 244, 244))
-            cv2.putText(image, "%.1f cm -- %.0f deg" % ((tvec[0][0][2] * 100), (rvec[0][0][2] / math.pi * 180)), (0, 230), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (244, 244, 244))
-  #          cv2.circle(image, (100, int(rr / 600)), 6, (200, 40, 230), -1)
-            R, _ = cv2.Rodrigues(rvec[0])
-            cameraPose = -R.T * tvec[0]
-            #print(type(cameraPose))
-            #print((int)(tvec[0][0][2] * 1000))
-            #rvec = red, blue, green            
-            """
-            if ((rvec[0][0][1])) > rvecmax:
-                rvecmax = (rvec[0][0][1]);
-            print((int)(rvec[0][0][0] / math.pi * 180), " ", (int)(rvec[0][0][1] / math.pi * 180), " ", (int)(rvec[0][0][2] / math.pi * 180))
-            #print(rvec.shape)
-            """
-    
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
-    rawCapture.truncate(0)
-    if key == ord("q"):
-        break
+if __name__ == '__main__':
+        start(sys.argv)
