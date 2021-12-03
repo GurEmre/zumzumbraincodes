41a97825da4dde3fca2695435ed2ece1b884f9cb
GurEmre <guremreme@gmail.com>
2021-12-03 05:46:45 +0300

    sfghsfgh

--- try/opencvzero.py
+++ try/opencvzero.py
@@ -71,7 +71,7 @@ def start(args):
         print(aruco_ids)
         if (aruco_ids != []):
             print("distance:")
-            print(tvecs)
+            print(tvecs(2))
         print(time.time() - start_time)
         time.sleep(1)
     cap.release()
