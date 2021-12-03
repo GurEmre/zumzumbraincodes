ad28f5864b5f9366ef66e98f7ff2974b9738da5d
GurEmre <guremreme@gmail.com>
2021-12-03 05:29:30 +0300

    dghsf

--- try/opencvzero.py
+++ try/opencvzero.py
@@ -73,7 +73,7 @@ def start(args):
         print(aruco_ids)
         if (aruco_ids != []):
             print("ONWARD!!!")
-            print(tvecs())
+            print(tvecs)
         print(time.time() - start_time)
         time.sleep(1)
     cap.release()
